3b40cc8f986f36efd44fa5e9998a87f0cd968694
Roman Novitsky <roman.novitsky@outlook.com>
2020-02-15 21:44:28 +0100

    Add project files.

---
+++ Projctl/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projctl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
