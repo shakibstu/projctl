4b97fac285b8f4f7de46e7a676ecc627ef209e26
Roman Novitsky <roman.novitsky@outlook.com>
2020-02-18 09:12:12 +0100

    Refactored tests

--- Projctl.Test/GetProjectsTest.cs
+++ Projctl.Test/GetProjectsTest.cs
@@ -14,35 +14,30 @@ namespace Projctl.Test
     [TestClass]
     public class GetProjectsTest : TestBase
     {
-        private const string _testProjectA = "TestSolution\\TestProjectA\\TestProjectA.csproj";
-        private const string _testProjectAClass1Path = "TestSolution\\TestProjectA\\Class1.cs";
-        private const string _testProjectAClass2Path = "TestSolution\\TestProjectA\\Class2.cs";
-        private const string _testSlnPath = "TestSolution\\Test.sln";
-
         [TestMethod]
         public async Task ShouldFindOneProjectContainingCompileProjectItemInSolution()
         {
             await InvokeAsync(
-                $"get-projects --solution {_testSlnPath} --containing-files {_testProjectAClass1Path}\n{_testProjectAClass2Path} --project-item-types Compile");
+                $"get-projects --solution {TestSlnPath} --containing-files {TestProjectAClass1Path}\n{TestProjectAClass2Path} --project-item-types Compile");
 
-            Out.Should().HaveCount(1).And.Contain(_testProjectA);
+            Out.Should().HaveCount(1).And.Contain(TestProjectA);
         }
 
         [TestMethod]
         public async Task ShouldFindOneProjectContainingFileInFolder()
         {
-            await InvokeAsync($"get-projects --containing-files {_testProjectAClass1Path}\n{_testProjectAClass2Path}");
+            await InvokeAsync($"get-projects --containing-files {TestProjectAClass1Path}\n{TestProjectAClass2Path}");
 
-            Out.Should().HaveCount(1).And.Contain(_testProjectA);
+            Out.Should().HaveCount(1).And.Contain(TestProjectA);
         }
 
         [TestMethod]
         public async Task ShouldFindOneProjectContainingFileInSolution()
         {
             await InvokeAsync(
-                $"get-projects --solution {_testSlnPath} --containing-files {_testProjectAClass1Path}\n{_testProjectAClass2Path}");
+                $"get-projects --solution {TestSlnPath} --containing-files {TestProjectAClass1Path}\n{TestProjectAClass2Path}");
 
-            Out.Should().HaveCount(1).And.Contain(_testProjectA);
+            Out.Should().HaveCount(1).And.Contain(TestProjectA);
         }
     }
 }
